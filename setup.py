fbe0b4f31d861e470bbf707eb338b60e508384ae
Feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2022-07-18 07:42:22 -0300

    update version and libs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='feiticeiro_tec',
-    version='0.0.0.1',
+    version='0.1',
     url='https://github.com/feiticeiro-tec/feiticeiro-tec',
     license='MIT License',
     author='Silvio Henrique Cruz Da Silva',
@@ -15,7 +15,7 @@ setup(
     keywords='Pacote',
     description=u'Um Pacote Das Mais Diversas Utilidades.',
     packages=['feiticeiro_tec'],
-    install_requires=[],
+    install_requires=['pyjwt'],
     extras_require={
         'server': [
             'flask',
