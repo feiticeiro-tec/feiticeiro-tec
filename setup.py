11a123c279e2a7155d10792bf2e92f9a2b4b949f
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-09-05 08:36:18 +0000

    update to version 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="feiticeiro_tec",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/feiticeiro-tec/feiticeiro-tec",
     license="BSD3",
     author="Silvio Henrique Cruz Da Silva",
