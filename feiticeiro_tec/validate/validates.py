5fa52114488692667005a689e5ace6781a20c073
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-09-05 06:58:08 +0000

    doc and fix

--- feiticeiro_tec/validate/validates.py
+++ feiticeiro_tec/validate/validates.py
@@ -4,21 +4,25 @@ from .types import CPFCNPJ, INCRICAOMUNICIPAL, EMAIL
 
 
 class Cpf(AbstractValidate):
+    CLEAR = True
     MSG_RAISE = "CPF inválido!"
     VALIDATOR = CPF
 
 
 class Cnpj(AbstractValidate):
+    CLEAR = True
     MSG_RAISE = "CNPJ inválido!"
     VALIDATOR = CNPJ
 
 
 class CpfCnpj(AbstractValidate):
+    CLEAR = True
     MSG_RAISE = "CPF ou CNPJ inválido!"
     VALIDATOR = CPFCNPJ
 
 
 class InscricaoMunicipal(AbstractValidate):
+    CLEAR = True
     MSG_RAISE = "Inscrição Municipal inválida!"
     VALIDATOR = INCRICAOMUNICIPAL
 
