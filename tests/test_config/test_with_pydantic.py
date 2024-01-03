625bb2fb2c42289aec8df0934010264e2d614704
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2024-01-03 17:50:09 +0000

    teste env com objeto

--- tests/test_config/test_with_pydantic.py
+++ tests/test_config/test_with_pydantic.py
@@ -18,8 +18,8 @@ def test_load_env():
     assert env.OLA == "mundo"
 
 
-def test_load_env_with_env():
-    os.environ["TESTE"] = '2'
+def test_load_env_with_env_forte():
+    os.environ["TESTE"] = "2"
 
     class Env(TypeEnv):
         TESTE: int
@@ -27,8 +27,26 @@ def test_load_env_with_env():
     env = Env.load_env(
         default="DEFAULT",
         env="DEV",
-        # settings_files=["tests/test_config/arquivos/teste_env.yaml"],
+        settings_files=["tests/test_config/arquivos/teste_env.yaml"],
     )
     assert env.ENV.value == "DEV"
     assert env.DEBUG is False
     assert env.TESTE == 2
+
+
+def test_load_env_with_objeto():
+    os.environ.pop("TESTE", None)
+
+    class Env(TypeEnv):
+        TESTE: int
+        OBJETO: dict
+
+    env = Env.load_env(
+        default="DEFAULT",
+        env="DEV",
+        settings_files=["tests/test_config/arquivos/teste_env.yaml"],
+    )
+    assert env.ENV.value == "DEV"
+    assert env.DEBUG is False
+    assert env.TESTE == 1
+    assert env.OBJETO == {"x": 2, "y": "Y"}
