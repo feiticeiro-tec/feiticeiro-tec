29b8a8d636025deeba751ed800a3ca5ce5e9614a
SilvioHC <silviohenriquecruzdasilva@gmail.com>
2024-01-04 02:52:33 +0000

    migração de view para flask admin page

--- tests/test_flask/test_job_manager/test_job_manager.py
+++ tests/test_flask/test_job_manager/test_job_manager.py
@@ -4,32 +4,14 @@ from flask import Flask
 
 def test_init_app():
     app = Flask(__name__)
-    app.secret_key = "123"
     job_manager = JobManager(app)
     assert app.extensions["job_manager"] == job_manager
-    assert job_manager.password_key == app.secret_key
-
-
-def test_endpoint_seguro():
-    app = Flask(__name__)
-    app.secret_key = "123"
-    JobManager(app)
-    with app.test_client() as client:
-        response = client.get("/jobs/")
-        assert response.status_code == 302
-        assert response.location == "/jobs/login/"
 
 
 def test_login():
     app = Flask(__name__)
     app.secret_key = "123"
-    JobManager(app)
+    JobManager(app, lambda: False)
     with app.test_client() as client:
-        response = client.get("/jobs/login/")
-        assert response.status_code == 200
-
-    with app.test_client() as client:
-        response = client.post("/jobs/login/", data={"acesso": "123"})
-        assert response.status_code == 302
-        assert response.location == "/jobs/"
-        assert client.get("/jobs/").status_code == 200
+        response = client.get("/admin/jobs/")
+        assert response.status_code == 403
--- tests/test_flask/test_job_manager/test_jobs.py
+++ tests/test_flask/test_job_manager/test_jobs.py
@@ -5,14 +5,12 @@ from multiprocessing import SimpleQueue
 
 def test_new_task():
     app = Flask(__name__)
-    app.secret_key = "123"
     jm = JobManager(app)
     processo = jm.tasks.new("TASK TASK BEM AQUI", lambda: print("teste"))
 
     with app.test_client() as client:
-        response = client.post(
-            "/jobs/login",
-            data={"acesso": "123"},
+        response = client.get(
+            "/admin/jobs/",
             follow_redirects=True,
         )
         assert str(processo.uuid) in response.text
@@ -33,12 +31,7 @@ def test_run_job():
     )
 
     with app.test_client() as client:
-        client.post(
-            "/jobs/login",
-            data={"acesso": "123"},
-            follow_redirects=True,
-        )
-        x = client.post(f"/jobs/{processo.uuid}/", follow_redirects=True)
+        x = client.post(f"/admin/jobs/{processo.uuid}/", follow_redirects=True)
         assert x.status_code == 200
 
     assert hook.get() == 182653
